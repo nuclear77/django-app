d3d14ba10545b7e50fa8cb4b89d71dcc4a6970fb
nuclear7777 <spiritvoideu@gmail.com>
2023-11-20 21:33:31 +0300

    added signals

--- app/test_app/signals.py
+++ app/test_app/signals.py
@@ -1,12 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Library
+from .models import Book
 
 
-@receiver(post_save, sender=Library)
+@receiver(post_save, sender=Book)
 def Item_created(sender, instance, created, **kwergs):
     if created:
-        print("была создана библиотека", instance.name)
+        print("была создана книга", instance.title)
     else:
-        print("библиотека обновлена", instance.name)
+        print("книга обновлена", instance.title)
 
